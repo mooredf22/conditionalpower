35cc5aa8cec3d682b3b06aa08beb356c53002384
Dirk F Moore <dirkfmoore@gmail.com>
2020-06-05 10:48:41 -0400

    some more setup commits

--- R/plotCondPower.r
+++ R/plotCondPower.r
@@ -5,7 +5,11 @@ plotCondPower <- function(condPowerOut=condPow) {
   # make a conditional power plot based on the output
   # of condPower
 
-  prob.signif=condPowerOut$prob.signif
+  oldpar <- par(no.readonly=TRUE)
+  on.exit(par(oldpar))
+
+  par(mar=c(4,4, 2, 4))
+  prob.signif <- condPowerOut$prob.signif
   tau=condPowerOut$tau
   B.tau=condPowerOut$B.tau
   theta=condPowerOut$theta
@@ -13,7 +17,7 @@ plotCondPower <- function(condPowerOut=condPow) {
 
   xx <- c(0,1)
   yy <- c(0, max(theta, B.predict))
-  plot(yy ~ xx, xlab="", ylab="", type="n", axes=F)
+  plot(yy ~ xx, xlab="tau", ylab="Conditional power", type="n", axes=F)
   segments(x0=0, y0=0, x1=1, y1=theta, lty=1)
   segments(x0=0, y0=0, x1=tau, y1=B.tau, col="blue")
   segments(x0=tau, y0=B.tau, x1=1, y1=B.predict, lty=2, col="blue")
@@ -22,6 +26,7 @@ plotCondPower <- function(condPowerOut=condPow) {
   points(x=tau, y=B.tau, pch=16, col="blue")
   points(x=1, y=B.predict, pch=16, col="blue")
   axis(1)
+  axis(2)
   axis(4, las=1)
 
 }
