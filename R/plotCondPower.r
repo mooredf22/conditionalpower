d114aa3ac21d3366d1d7e85a727213664a392bc3
Dirk F Moore <dirkfmoore@gmail.com>
2020-06-05 21:37:07 -0400

    minor fixes

--- R/plotCondPower.r
+++ R/plotCondPower.r
@@ -14,20 +14,20 @@ plotCondPower <- function(condPowerOut=condPow) {
   B.tau=condPowerOut$B.tau
   theta=condPowerOut$theta
   B.predict=condPowerOut$B.predict
-  B.predict.origTrend <- B.tau/tau
+  B.predict.currentTrend <- B.tau/tau
 
   xx <- c(0,1)
-  yy <- c(0, max(theta, B.predict, B.predict.origTrend))
+  yy <- c(0, max(theta, B.predict, B.predict.currentTrend))
   plot(yy ~ xx, xlab="tau", ylab="Theta", type="n", axes=F)
   segments(x0=0, y0=0, x1=1, y1=theta, lty=1, lwd=1.5)
   segments(x0=0, y0=0, x1=tau, y1=B.tau,lwd=1.5,  col="blue")
   segments(x0=tau, y0=B.tau, x1=1, y1=B.predict, lty=2, lwd=1.5, col="blue")
-  segments(x0=tau, y0=B.tau, x1=1, y1=B.predict.origTrend, lty=3, lwd=1.5, col="blue")
+  segments(x0=tau, y0=B.tau, x1=1, y1=B.predict.currentTrend, lty=3, lwd=1.5, col="blue")
 
   points(x=1, y=theta, pch=17, cex=1.5)
   points(x=tau, y=B.tau, pch=16, col="blue", cex=1.5)
   points(x=1, y=B.predict, pch=15, col="blue", cex=1.5)
-  points(x=1, y=B.predict.origTrend, pch=18, col="blue", cex=1.5)
+  points(x=1, y=B.predict.currentTrend, pch=18, col="blue", cex=1.5)
 
   axis(1)
   axis(2)
